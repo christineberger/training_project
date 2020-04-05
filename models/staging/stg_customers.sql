66f1a7ffba5f897abf544da13a4251981d2b2334
Christine <christinebergersd@gmail.com>
2020-04-05 00:16:35 -0500

    Renamed columns in stg models

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,5 +1,24 @@
 with source as (
     select * from {{ source('dbt_christine', 'customers') }}
+),
+
+renamed as (
+    select
+        -- Keys
+        id as customer_id,
+        
+        -- Strings
+        email,
+        first_name,
+        last_name,
+        gender,
+        
+        -- Boolean
+        accepts_marketing,
+        
+        -- Dates
+        created_at::date as created_date
+    from source
 )
 
-select * from source
+select * from renamed
